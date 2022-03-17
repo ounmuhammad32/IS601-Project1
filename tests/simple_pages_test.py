14816ff8e6064b88479fe2a5edd7d50490a96c11
ounmuhammad32 <45170041+ounmuhammad32@users.noreply.github.com>
2022-03-16 22:12:23 -0400

    updated files

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,10 +5,7 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">GIT and Github</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Python and Flask</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">CICD</a>' in response.data
+
 
 def test_request_index(client):
     """This makes the index page"""
